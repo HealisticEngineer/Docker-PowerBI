68b1fb03b75cfe13e1a123690cc8ddc4c5b84709
HealisticEngineer <johnedwardhall@gmail.com>
2022-01-14 19:59:46 +0100

    Update to dynamic code
    update to dynamic code

--- pbiinstall.ps1
+++ pbiinstall.ps1
@@ -1,3 +1,7 @@
+# Get latetst PowerBI Report Server
+$url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=57270"
+$PBRS = (iwr $url -UseBasicParsing).links | Where-Object {$_ -match "https://download.microsoft.com/download"} 
+$ENV:PowerBI  = ($PBRS | Where-Object {$_.href -match "Powerbireportserver.exe"} |Select-Object -first 1).href
 # Install PowerBI
 new-item -Path C:\ -Name PBIRS_TEMP -ItemType Directory
 (New-Object System.Net.WebClient).DownloadFile($ENV:PowerBI, "c:\PBIRS_TEMP\PowerBIReportServer.exe")
